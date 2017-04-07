27ad11714f92218f48948226ae2096b1d17b1a15
Matthieu Dorier <mdorier@anl.gov>
2017-04-07 15:37:23 -0500

    fixed indentation

--- argobots/01_threads/01_threads.c
+++ argobots/01_threads/01_threads.c
@@ -5,46 +5,48 @@
 
 void thread_hello(void *arg)
 {
-    printf("ULT%d: Hello, world!\n", (int)(size_t)arg);
+		int rank;
+		ABT_xstream_self_rank(&rank);
+		printf("ULT %d in XSTREAM %d: Hello, world!\n", (int)(size_t)arg, rank);
 }
 
 int main(int argc, char *argv[])
 {
-    ABT_xstream xstreams[NUM_XSTREAMS];
-    ABT_pool    pools[NUM_XSTREAMS];
-    ABT_thread  threads[NUM_XSTREAMS];
-    size_t i;
+		ABT_xstream xstreams[NUM_XSTREAMS];
+		ABT_pool    pools[NUM_XSTREAMS];
+		ABT_thread  threads[NUM_XSTREAMS];
+		size_t i;
 
-    ABT_init(argc, argv);
+		ABT_init(argc, argv);
 
-    /* Execution Streams */
-    ABT_xstream_self(&xstreams[0]);
-    for (i = 1; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
-    }
+		/* Execution Streams */
+		ABT_xstream_self(&xstreams[0]);
+		for (i = 1; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
+		}
 
-    /* Get the first pool associated with each ES */
-    for (i = 0; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
-    }
+		/* Get the first pool associated with each ES */
+		for (i = 0; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
+		}
 
-    /* Create ULTs */
-    for (i = 0; i < NUM_XSTREAMS; i++) {
-        ABT_thread_create(pools[i], thread_hello, (void *)i,
-                          ABT_THREAD_ATTR_NULL, &threads[i]);
-    }
+		/* Create ULTs */
+		for (i = 0; i < NUM_XSTREAMS; i++) {
+				ABT_thread_create(pools[i], thread_hello, (void *)i,
+								ABT_THREAD_ATTR_NULL, &threads[i]);
+		}
 
-    /* Join & Free */
-    for (i = 0; i < NUM_XSTREAMS; i++) {
-        ABT_thread_join(threads[i]);
-        ABT_thread_free(&threads[i]);
-    }
-    for (i = 1; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_join(xstreams[i]);
-        ABT_xstream_free(&xstreams[i]);
-    }
+		/* Join & Free */
+		for (i = 0; i < NUM_XSTREAMS; i++) {
+				ABT_thread_join(threads[i]);
+				ABT_thread_free(&threads[i]);
+		}
+		for (i = 1; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_join(xstreams[i]);
+				ABT_xstream_free(&xstreams[i]);
+		}
 
-    ABT_finalize();
+		ABT_finalize();
 
-    return 0;
+		return 0;
 }
--- argobots/02_tasks/02_tasks.c
+++ argobots/02_tasks/02_tasks.c
@@ -6,40 +6,40 @@
 
 void task_hello(void *arg)
 {
-    printf("TASK%d: Hello, world!\n", (int)(size_t)arg);
+		printf("TASK%d: Hello, world!\n", (int)(size_t)arg);
 }
 
 int main(int argc, char *argv[])
 {
-    ABT_xstream xstreams[NUM_XSTREAMS];
-    ABT_pool    pools[NUM_XSTREAMS];
-    size_t i;
+		ABT_xstream xstreams[NUM_XSTREAMS];
+		ABT_pool    pools[NUM_XSTREAMS];
+		size_t i;
 
-    ABT_init(argc, argv);
+		ABT_init(argc, argv);
 
-    /* Execution Streams */
-    ABT_xstream_self(&xstreams[0]);
-    for (i = 1; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
-    }
+		/* Execution Streams */
+		ABT_xstream_self(&xstreams[0]);
+		for (i = 1; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
+		}
 
-    /* Get the first pool associated with each ES */
-    for (i = 0; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
-    }
+		/* Get the first pool associated with each ES */
+		for (i = 0; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_get_main_pools(xstreams[i], 1, &pools[i]);
+		}
 
-    /* Create Tasks */
-    for (i = 0; i < (NUM_XSTREAMS*TASKS_PER_XTREAM); i++) {
-		ABT_task_create(pools[i % NUM_XSTREAMS], task_hello, (void *)i, NULL);
-    }
+		/* Create Tasks */
+		for (i = 0; i < (NUM_XSTREAMS*TASKS_PER_XTREAM); i++) {
+				ABT_task_create(pools[i % NUM_XSTREAMS], task_hello, (void *)i, NULL);
+		}
 
-    /* Free */
-    for (i = 1; i < NUM_XSTREAMS; i++) {
-        ABT_xstream_join(xstreams[i]);
-        ABT_xstream_free(&xstreams[i]);
-    }
+		/* Free */
+		for (i = 1; i < NUM_XSTREAMS; i++) {
+				ABT_xstream_join(xstreams[i]);
+				ABT_xstream_free(&xstreams[i]);
+		}
 
-    ABT_finalize();
+		ABT_finalize();
 
-    return 0;
+		return 0;
 }
