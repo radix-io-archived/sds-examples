d1732bcf8221e81d991cb53e356529a75caad531
Matthieu Dorier <mdorier@anl.gov>
2017-04-08 14:56:45 -0500

    added some comments

--- argobots/01_threads/01_threads.c
+++ argobots/01_threads/01_threads.c
@@ -3,11 +3,13 @@
 
 #define NUM_XSTREAMS    4
 
+/* This is the function executed by each of the threads */
 void thread_hello(void *arg)
 {
-		int rank;
-		ABT_xstream_self_rank(&rank);
-		printf("ULT %d in XSTREAM %d: Hello, world!\n", (int)(size_t)arg, rank);
+	/* Get the rank of the ES */
+	int rank;
+	ABT_xstream_self_rank(&rank);
+	printf("ULT %d in XSTREAM %d: Hello, world!\n", (int)(size_t)arg, rank);
 }
 
 int main(int argc, char *argv[])
@@ -17,9 +19,12 @@ int main(int argc, char *argv[])
 		ABT_thread  threads[NUM_XSTREAMS];
 		size_t i;
 
+		/* Initialize Argobots */
 		ABT_init(argc, argv);
 
-		/* Execution Streams */
+		/* Execution Streams
+		 * xtrseam[0] will be the current ES, no need to create it.
+		 */
 		ABT_xstream_self(&xstreams[0]);
 		for (i = 1; i < NUM_XSTREAMS; i++) {
 				ABT_xstream_create(ABT_SCHED_NULL, &xstreams[i]);
@@ -46,6 +51,7 @@ int main(int argc, char *argv[])
 				ABT_xstream_free(&xstreams[i]);
 		}
 
+		/* Finalize */
 		ABT_finalize();
 
 		return 0;
