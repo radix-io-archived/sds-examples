787193cd58a13ebd12abb3539b66f1c1b74c22c3
Matthieu Dorier <mdorier@anl.gov>
2017-04-08 18:10:05 -0500

    added CCI and GNI support

--- cmake/Modules/FindBMI.cmake
+++ cmake/Modules/FindBMI.cmake
@@ -1,29 +1,23 @@
 # - Try to find BMI
-# Once done, this will define
-#
-#  BMI_FOUND - system has BMI
-#  BMI_INCLUDE_DIRS - the BMI include directories
-#  BMI_LIBRARIES - link these to use BMI
+# Once done this will define
+#  BMI_FOUND - System has BMI
+#  BMI_INCLUDE_DIRS - The BMI include directories
+#  BMI_LIBRARIES - The libraries needed to use BMI
 
-# Include dir
-find_path(BMI_INCLUDE_DIR
-  bmi.h
-  HINTS ${BMI_ROOT}/include
-)
+find_path(BMI_INCLUDE_DIR bmi.h
+  HINTS /usr/local/include /usr/include ${BMI_ROOT}/include)
 
-# Finally the library itself
-find_library(BMI_LIBRARY
-  bmi
-  HINTS ${BMI_ROOT}/lib
-)
+find_library(BMI_LIBRARY NAMES bmi
+  PATHS /usr/local/lib /usr/lib
+  HINTS ${BMI_ROOT}/lib)
 
-set(BMI_LIBRARIES ${BMI_LIBRARY})
 set(BMI_INCLUDE_DIRS ${BMI_INCLUDE_DIR})
+set(BMI_LIBRARIES ${BMI_LIBRARY})
 
 include(FindPackageHandleStandardArgs)
 # handle the QUIETLY and REQUIRED arguments and set BMI_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args(BMI  DEFAULT_MSG
-                                  BMI_LIBRARY BMI_INCLUDE_DIR)
+find_package_handle_standard_args(BMI DEFAULT_MSG
+                                  BMI_INCLUDE_DIR BMI_LIBRARY)
 
 mark_as_advanced(BMI_INCLUDE_DIR BMI_LIBRARY)
